281078a5d094f84834f2e44b55dad369dfbe9b6c
vibi1177 <vibi1177@gmail.com>
2018-07-19 15:33:16 +0200

    test per link al prodotto di dettaglio

--- magnoliaspace/spiders/magnoliaspace.py
+++ magnoliaspace/spiders/magnoliaspace.py
@@ -6,9 +6,16 @@ class MagnoliaspaceSpider(scrapy.Spider):
     name = "magnoliaspace"
     allowed_domains = ["www.magnoliaspace.it"]
     start_urls = [
-        'https://www.magnoliaspace.it/',
+        'https://www.magnoliaspace.it/catalog/section/38/i-prodotti.htm',
     ]
 
     def parse(self, response):
-        yield{'title' : response.css('title::text').extract_first()}
+        prspan = response.xpath(
+            '//div[@class="coldx"]/div[@class="contcategory"]/span/div[@class="s-prod"]'
+        )
+        for prod in prspan:
+            linktoprod = prod.xpath('.//div[@class="contfoto"]/a/@href').extract_first()
+            #yield scrapy.Request(response.urljoin(linktoprod), callback=self.parse_prdetail)
+            yield linktoprod
+
 
